c328af564e5b2568a0fc2b59452cc68d23350214
nigelnix <nigel.nix@outlook.com>
2024-03-18 10:52:26 +0100

    UTC date in backend tracking model for consistency

--- index.js
+++ index.js
@@ -159,6 +159,7 @@ app.post(`/tracking`, verifyToken, async (req, res) => {
 app.get(`/tracking/:userid/:date`, async (req, res) => {
   let userid = req.params.userid;
   let date = moment.utc(req.params.date, "DD/MM/YYYY").startOf('day'); // Convert to UTC and start of day
+  console.log("Date sent to backend", date)
   try {
     let foods = await trackingModel
       .find({ userId: userid, eatenDate: date })
