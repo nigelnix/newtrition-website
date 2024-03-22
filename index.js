367296f9bc355f89410c4251a3460609c4243893
nigelnix <nigel.nix@outlook.com>
2024-03-22 19:43:38 +0100

    added verifyToken middleware to server date tracking endpoint

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ app.post(`/tracking`, verifyToken, async (req, res) => {
 
 // endpoint to fetch all foods eaten by a person
 
-app.get(`/tracking/:userid/:date`, async (req, res) => {
+app.get(`/tracking/:userid/:date`, verifyToken,  async (req, res) => {
   let userid = req.params.userid;
   let date = moment.utc(req.params.date, "DD/MM/YYYY").startOf('day'); // Convert to UTC and start of day
   console.log("Date sent to backend", date)
